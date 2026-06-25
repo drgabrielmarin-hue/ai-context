fee9058903c06bf955c4fb310c4a32e6bffd3c6e
DrGabrielMarin <drgabrielmarin@gmail.com>
2026-06-25 14:06:57 -0300

    sinc go

--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/tanq16/ai-context/cmd"
-
-func main() {
-	cmd.Execute()
-}
--- utils/globals.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-// GlobalDebugFlag is set by cobra root command when --debug is passed
-var GlobalDebugFlag bool
-
-// GlobalForAIFlag is set by cobra root command when --for-ai is passed
-// When true, output uses plain text with prefixes and input reads from stdin pipe
-var GlobalForAIFlag bool
